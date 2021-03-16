024c9b4c58a488cd622ad402bf8ebda7070554eb
Antonov-S <65724927+Antonov-S@users.noreply.github.com>
2021-03-16 21:30:43 +0200

    04. Entity-Relations-Exercises

---
+++ Entity Relations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        // create constructors
+        // db sets
+        // connect sql server
+        // fluent API relations - many to many
+
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Проверка дали е конфигурирано за да не го прави всеки път.            
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=T450\SQLEXPRESS;Database=StudentSystem;Integrated Security=True");
+            }
+            
+            
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>(x =>
+            {
+                x.HasKey(x => new { x.CourseId, x.StudentId });
+            });
+            
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
